fef665e83c505564a0111c1c130929d224600c03
Nitin Tulswani <tulswani19@gmail.com>
2017-02-02 00:05:46 +0530

    all the links

---
+++ src/components/utility.js
@@ -0,0 +1,30 @@
+export const bbc = `https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const talk_sport = `https://newsapi.org/v1/articles?source=talksport&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const business_insider = `https://newsapi.org/v1/articles?source=business-insider&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const reddit = `https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const reddit_top = `https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const techcrunch = `https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const hacker_news = `https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const hacker_news_latest = `https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const polygon = ` https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const theVerge = `https://newsapi.org/v1/articles?source=the-verge&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const theVerge_top = `https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const insider_uk = `https://newsapi.org/v1/articles?source=financial-times&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const sky_sports = ` https://newsapi.org/v1/articles?source=the-sport-bible&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const engadget = `https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const ign = `https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const entertainment = `https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`
+export const lad = `https://newsapi.org/v1/articles?source=the-lad-bible&sortBy=latest&apiKey=6685a5bc333c419c8fa71716bc3a068c`
+export const four_four_two = ` https://newsapi.org/v1/articles?source=four-four-two&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const nat = `https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=6685a5bc333c419c8fa71716bc3a068c`;
+export const facebook = `https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/online_social_media_facebook-128.png`;
+export const twitter = `https://cdn3.iconfinder.com/data/icons/picons-social/57/43-twitter-128.png`;
+export const instagram = `https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/instagram_circle-128.png`;
+export const medium = `https://cdn4.iconfinder.com/data/icons/black-white-social-media/32/social_media_logo_medium-128.png`;
+export const github = `https://cdn2.iconfinder.com/data/icons/black-white-social-media/64/social_media_logo_github-128.png`;
+export const heart = `https://cdn2.iconfinder.com/data/icons/christmas-hand-drawn-scribbles-icons/512/88-128.png`;
+export const fb_profile = `https://www.facebook.com/Niki.dps`
+export const tw_profile = `https://twitter.com/NTulswani`
+export const ins_profile = `https://www.instagram.com/nitintulswani/`
+export const med_profile = `https://medium.com/@NTulswani`
+export const git_profile = `https://github.com/nitin42`
