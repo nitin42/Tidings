880dca5bdb97bb2c0131849e875d67830c3b68ea
Nitin Tulswani <tulswani19@gmail.com>
2017-02-01 23:56:55 +0530

    removed LOAD_MORE

---
+++ src/constants/SearchActionTypes.js
@@ -0,0 +1,6 @@
+export const SEARCH_PENDING = "SEARCH_PENDING";
+export const SEARCH_DONE = "SEARCH_DONE";
+export const IDLE = "IDLE";
+export const LOADING = "LOADING";
+export const LOADED = "LOADED";
+export const INIT = "INIT";
