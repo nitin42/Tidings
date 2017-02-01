d4e32ea72fd9bba980d7c373e323fa82a06c03bf
Nitin Tulswani <tulswani19@gmail.com>
2017-02-02 00:07:29 +0530

    ll be changed

---
+++ src/components/News/Gaming.js
@@ -0,0 +1,6 @@
+import HOC from '../HOC';
+import {polygon} from '../utility';
+
+let Gaming = () => {return null;}
+
+export default HOC(Gaming, polygon);
---
+++ src/components/News/Reddits.js
@@ -0,0 +1,6 @@
+import HOC from '../HOC';
+import {reddit} from '../utility';
+
+let Reddit = () => {return null;}
+
+export default HOC(Reddit, reddit);
